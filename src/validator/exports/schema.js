55d50bbd9fe59c1750a31d2e97ae0a752b598bb0
Aghony <Kerenjustin286@gmail.com>
2025-03-20 10:48:26 +0700

    export notes implemented

---
+++ src/validator/exports/schema.js
@@ -0,0 +1,7 @@
+const Joi = require('joi');
+
+const ExportNotesPayloadSchema = Joi.object({
+  targetEmail: Joi.string().email({ tlds: { allow: false } }).required(),
+});
+
+module.exports = ExportNotesPayloadSchema;
